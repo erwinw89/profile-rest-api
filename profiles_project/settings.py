c5a2161ef2b25456a91fec7dd2a43cd09026f174
erwin <rwn255@gmail.com>
2021-02-07 15:13:54 +0700

    create django project

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8py982d&^gy%)(v2d%w9omzl-1+x#qx1a@2%t!5lapk7s2%=8y'
+SECRET_KEY = 'dy153l5am(d0eojn!ob89539c!4o2i)+ry+c&+jsj7t16awdc%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
